b0e5fc60d9349024d26caa6751a5d8b744e07a49
Derek Gonyeo <derek.gonyeo@coreos.com>
2016-09-08 15:20:27 -0400

    code/router: should now respond to status update requests

--- code/router/router.ino
+++ code/router/router.ino
@@ -3,6 +3,31 @@
 // The ID of this arduino
 #define MY_ID 3
 
+struct StatusUpdate {
+    uint8_t message;
+    uint8_t data;
+};
+
+int numStatusUpdates = 0;
+StatusUpdate *statusUpdates = NULL;
+
+void queueStatusUpdate(StatusUpdate u) {
+    numStatusUpdates++;
+    statusUpdates = (StatusUpdate *)realloc(statusUpdates, numStatusUpdates);
+    memcpy(&statusUpdates[numStatusUpdates-1], &u, sizeof(StatusUpdate));
+}
+
+StatusUpdate getStatusUpdate() {
+    StatusUpdate u;
+    memcpy(statusUpdates, &u, sizeof(StatusUpdate));
+
+    numStatusUpdates--;
+    memcpy(statusUpdates, statusUpdates++, numStatusUpdates * sizeof(StatusUpdate));
+    statusUpdates = (StatusUpdate *)realloc(statusUpdates, numStatusUpdates);
+
+    return u;
+}
+
 // setup, as per arduino tooling, runs once when the arduino starts.
 void setup()
 {
@@ -34,6 +59,24 @@ void loop()
 
     switch(msg.message) {
         case STATUS_UPDATE:
+            if(numStatusUpdates == 0) {
+                writeMsg(
+                    {
+                        .id = MY_ID,
+                        .message = NOTHING,
+                        .data = 0
+                    }
+                );
+            } else {
+                StatusUpdate s = getStatusUpdate();
+                writeMsg(
+                    {
+                        .id = MY_ID,
+                        .message = s.message,
+                        .data = s.data
+                    }
+                );
+            }
             break;
         case CHANGE_TO_TUBE_0:
             break;
